cf2df57a9e683c7253b8194ed2b807ccd168824e
manu <144843962+ManusolJ@users.noreply.github.com>
2024-09-06 20:15:00 +0200

    Initial commit

---
+++ src/main/java/edu/es/eoi/projecteoi/ProjectEOIApplication.java
@@ -0,0 +1,13 @@
+package edu.es.eoi.projecteoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEoiApplication.class, args);
+	}
+
+}
